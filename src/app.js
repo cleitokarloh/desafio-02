83bcaaa9880b19bdd4018aee9eef7c4e0ff64be4
Cleito Karloh <cleito.kp@gmail.com>
2020-08-18 21:19:59 -0300

    Listar, cadastrar e editar repositorio.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+ const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,15 +10,45 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' } );
+  }
+
+  const repository = {
+    ...repositories[repositoryIndex],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(200).json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
