0f090a5a4ab3e749a525a6c5ed7d56a5ac341a5e
Cleito Karloh <cleito.kp@gmail.com>
2020-08-20 20:34:42 -0300

    feat(repository): Possibilidade de deletar repositório e adicionar likes

--- src/app.js
+++ src/app.js
@@ -52,11 +52,35 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' } );
+  }
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' } );
+  }
+
+  const repository = {
+    ...repositories[repositoryIndex],
+    likes: repositories[repositoryIndex].likes + 1,
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(201).json(repository);
 });
 
 module.exports = app;
